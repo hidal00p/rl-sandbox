7c9536d104b2581342bd90de694a0dda32e42d0e
hidal00p <anton.kov.hack@proton.me>
2022-08-02 10:33:08 +0200

    Makes a simple import instaed of wrapping

--- algo/others.py
+++ algo/others.py
@@ -1,5 +1,1 @@
-from gym.envs.box2d.lunar_lander import heuristic as daddy_heuristic
-
-def heuristic(env, s):
-    return daddy_heuristic(env, s)
-
+from gym.envs.box2d.lunar_lander import heuristic
